10cea3b8c4d7550e22ce9fb95129583fedd47f9e
angrycarp <khmg525@naver.com>
2023-05-30 15:06:42 +0900

    변수 기본형 및 선언 예제

---
+++ java_HM/src/day1/varialbe/IntExample1.java
@@ -0,0 +1,18 @@
+package day1.varialbe;
+
+public class IntExample1 {
+
+	public static void main(String[] args) {
+		
+		int num2 = 11;   // 십진수 11
+		int num3 = 0b11; // 2진수 11 == 3
+		int num4 = 011;  // 8진수 11 == 9
+		int num5 = 0x11; // 16진수 11 == 17
+		
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+
+	}
+}
