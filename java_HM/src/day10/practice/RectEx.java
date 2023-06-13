d48458cef85d134e7118ad2d4fba302ce08f93dd
angrycarp <khmg525@naver.com>
2023-06-13 14:40:24 +0900

    day10.pm

--- java_HM/src/day10/practice/RectEx.java
+++ java_HM/src/day10/practice/RectEx.java
@@ -24,38 +24,13 @@ class Rectanger{
 		this.y2 = y2;
 	}
 
-	// getter, setter
-	public int getX1() {
-		return x1;
-	}
-	public void setX1(int x1) {
-		this.x1 = x1;
-	}
-	public int getY1() {
-		return y1;
-	}
-	public void setY1(int y1) {
-		this.y1 = y1;
-	}
-	public int getX2() {
-		return x2;
-	}
-	public void setX2(int x2) {
-		this.x2 = x2;
-	}
-	public int getY2() {
-		return y2;
-	}
-	public void setY2(int y2) {
-		this.y2 = y2;
-	}
 	/** 두 점을 이용해 만들어진 사각형의 좌표를 출력하는 메서드
-	 * 	매개변수	: 
+	 * 	매개변수	: 없음
 	 * 	리턴타입	: 없음 => void
 	 * 	함수명 	: printRectanger 
 	 */
 	public void printRectanger() {
-		System.out.println("(" + x1 + " , " + y1 + ") " + "(" + x1 + " , " + y2 + ") "
-	+ "(" + x2 + " , " + y1 + ") " + "(" + x2 + " , " + y2 + ")");
+		System.out.println("(" + x1 + " , " + y1 + ") " + "(" + x1 + " , " + y2 + ") " + 
+	"(" + x2 + " , " + y1 + ") " + "(" + x2 + " , " + y2 + ")");
 	}
 }
